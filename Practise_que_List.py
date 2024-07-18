ce1dae514a9f72160149565679166aa6309e6d90
keshav Sharma <118153592+SnowyKeshav@users.noreply.github.com>
2024-07-18 11:07:26 +0530

    Update Practise_que_List.py

--- Practise_que_List.py
+++ Practise_que_List.py
@@ -1,7 +1,9 @@
+#Creating a List 
 list1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+#Printing the Elements 
 print(list1[19:0:-1]) 
 
-
+#Using For Loop
 for element in list1[::-1]:
     print(element)
 
@@ -10,6 +12,8 @@ for element in list1[::-1]:
 my_list = [1, 2, 3, 4, 5]
 sequence = ['a', 'b', 'c']
 index = 2
+#Enumerating List 
 for i, element in enumerate(sequence):
      my_list.insert(index + i, element)
+    #Printing the final output
  print(my_list)
